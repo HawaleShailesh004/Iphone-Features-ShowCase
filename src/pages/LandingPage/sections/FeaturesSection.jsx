a0e4dea0d2356d463fa0f77245e44544214f002d
Shailesh <shaileshhawale004@gmail.com>
2025-08-29 00:32:46 +0530

    Mobile Responsive

--- src/pages/LandingPage/sections/FeaturesSection.jsx
+++ src/pages/LandingPage/sections/FeaturesSection.jsx
@@ -75,17 +75,20 @@ const FeaturesSection = () => {
       // The class logic remains correct
       className={`${
         locked ? "sticky top-0" : "relative"
-      } w-full h-screen grid grid-cols-3 place-items-center gap-8 px-8 mb-20`}
+      } w-full min-h-screen grid grid-cols-1 md:grid-cols-3 place-items-center gap-8 px-8 mb-20`}
     >
       {/* Left Section: Feature Details */}
-      <div className="flex flex-col gap-6 justify-start items-start w-[320px] animate-fadeIn" key={currentFeature.title}>
-        <h1 className="text-2xl font-bold text-accent mb-7">
+      <div
+        className="grid gap-3 md:gap-6 justify-start items-start w-[320px] animate-fadeIn"
+        key={currentFeature.title}
+      >
+        <h1 className="text-2xl font-bold text-accent mt-5 md:mb-7">
           {`Feature No: ${activeIndex + 1}`}
         </h1>
-        <h2 className="text-2xl font-semibold text-primary uppercase mb-5">
+        <h2 className="text-2xl font-semibold text-primary uppercase md:mb-5">
           {currentFeature.title}
         </h2>
-        <ul className="space-y-1 text-secondary-dark leading-relaxed mb-15">
+        <ul className="space-y-1 text-secondary-dark leading-relaxed md:mb-15">
           {currentFeature.description.map((point, index) => (
             <li key={index} className="flex items-start gap-2 text-sm">
               <span className="w-1 h-1 mt-2 rounded-full bg-secondary-light"></span>
@@ -94,7 +97,7 @@ const FeaturesSection = () => {
           ))}
         </ul>
         {/* Navigation Arrows */}
-        <div className="flex items-center justify-between gap-2 mt-6 h-10">
+        <div className="flex w-15 md:w-auto items-center justify-between gap-2 mt-6 h-10">
           <button
             className="cursor-pointer text-xl"
             onClick={() => setActiveIndex((prev) => Math.max(prev - 1, 0))}
@@ -114,15 +117,15 @@ const FeaturesSection = () => {
       </div>
 
       {/* Center Section: iPhone Mockup */}
-      <div className="relative w-[320px]">
+      <div className="relative w-full md:w-[320px] flex justify-center items-center mx-auto">
         <img
-          className="w-[90%] h-[90%]"
+          className="w-[55%] h-[95%] md:w-[90%] md:h-[90%]"
           src="/images/iphone-mockup.png"
           alt="iPhone Mockup"
         />
         <img
           key={currentFeature.image}
-          className="absolute top-[2.5%] left-[8%] w-[75%] h-[95%] rounded-[30px] overflow-hidden animate-fadeIn"
+          className="absolute top-[2.5%] left-1/2 transform -translate-x-1/2 w-[46%] h-[95%] md:w-[75%] md:h-[95%] rounded-[10px] md:rounded-[30px] overflow-hidden animate-fadeIn"
           src={currentFeature.image}
           alt={currentFeature.title}
         />
@@ -131,11 +134,11 @@ const FeaturesSection = () => {
       {/* Right Section */}
       <div className="flex flex-col items-start w-[320px]">
         <h2 className="text-2xl font-bold text-primary">Feature Showcase</h2>
-        <ul className="list-none mt-5 text-secondary-light w-full">
+        <ul className="list-none md:mt-5 mt-3 text-secondary-light w-full">
           {featureList.map((feature, index) => (
             <li
               key={index}
-              className={`flex items-center rounded min-h-[60px] cursor-pointer w-full ${
+              className={`flex items-center rounded min-h-[20px]md:min-h-[60px] cursor-pointer w-full ${
                 activeIndex === index ? "font-bold text-primary" : "" // Added text-primary for better visibility
               }`}
               onClick={() => setActiveIndex(index)}
